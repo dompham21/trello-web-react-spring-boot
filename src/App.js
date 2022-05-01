50f3df20a454be45c4f8f1c20417c4f463a9620b
dompham21 <63381043+dompham21@users.noreply.github.com>
2022-05-01 17:46:04 +0700

    Design main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,120 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="main">
+      <nav className="navbar app">Navbar</nav>
+      <nav className="navbar board"> board nav</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>
+            <h2>Braindstorm</h2>
+          </header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" 
+              alt="title-here-alt" />
+              Title SMT
+            </li>
+            <li>
+              I've drafted my goals for the next few months. Any feedback?
+            </li>
+          </ul>
+          <footer>
+            <span>Add a card</span>
+           </footer>
+        </div>
+        <div className="column">
+          <header>
+            <h2>Braindstorm</h2>
+          </header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" 
+              alt="title-here-alt" />
+              Title SMT
+            </li>
+            <li>
+              I've drafted my goals for the next few months. Any feedback?
+            </li>
+          </ul>
+          <footer>
+            <span>Add a card</span>
+           </footer>
+        </div>
+        <div className="column">
+          <header>
+            <h2>Braindstorm</h2>
+          </header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" 
+              alt="title-here-alt" />
+              Title SMT
+            </li>
+            <li>
+              I've drafted my goals for the next few months. Any feedback?
+            </li>
+          </ul>
+          <footer>
+            <span>Add a card</span>
+           </footer>
+        </div>
+        <div className="column">
+          <header>
+            <h2>Braindstorm</h2>
+          </header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" 
+              alt="title-here-alt" />
+              Title SMT
+            </li>
+            <li>
+              I've drafted my goals for the next few months. Any feedback?
+            </li>
+          </ul>
+          <footer>
+            <span>Add a card</span>
+           </footer>
+        </div>
+        <div className="column">
+          <header>
+            <h2>Braindstorm</h2>
+          </header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" 
+              alt="title-here-alt" />
+              Title SMT
+            </li>
+            <li>
+              I've drafted my goals for the next few months. Any feedback?
+            </li>
+          </ul>
+          <footer>
+            <span>Add a card</span>
+           </footer>
+        </div>
+        <div className="column">
+          <header>
+            <h2>Braindstorm</h2>
+          </header>
+          <ul>
+            <li>
+              <img src="https://picsum.photos/200/300" 
+              alt="title-here-alt" />
+              Title SMT
+            </li>
+            <li>
+              I've drafted my goals for the next few months. Any feedback?
+            </li>
+          </ul>
+          <footer>
+            <span>Add a card</span>
+           </footer>
+        </div>
+      </div>
     </div>
   );
 }
